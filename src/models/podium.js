f057ef88f52376e621c0ba6c56e7fc016b1b0336
pooya <poooyasamimi@gmail.com>
2025-09-18 18:16:28 +0330

    first

---
+++ src/models/podium.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+
+export function loadPodium(gltfLoader, scene, textureLoader) {
+  const rubberColorTex = textureLoader.load('/textures/rubber/Color.jpg')
+  const rubberRoughnessTex = textureLoader.load('/textures/rubber/Roughness.jpg')
+  const rubberNormalMapTex = textureLoader.load('/textures/rubber/NormalGL.jpg')
+
+  gltfLoader.load(
+    '/podium.glb',
+    function (gltf) {
+      const podiumModel = gltf.scene
+      scene.add(podiumModel)
+
+      podiumModel.traverse(child => {
+        if (child.isMesh) {
+          child.material = new THREE.MeshStandardMaterial({
+            map: rubberColorTex,
+            roughness: rubberRoughnessTex,
+            normalMap: rubberNormalMapTex,
+          })
+        }
+      })
+    },
+    undefined,
+    function (error) {
+      alert('Podium load error')
+    }
+  )
+}
