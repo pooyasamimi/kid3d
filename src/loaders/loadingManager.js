24874b152866cff0c591a2805134c2fec1d024cb
pooya <poooyasamimi@gmail.com>
2025-09-19 16:25:39 +0330

    add onerror func to loading manager

--- src/loaders/loadingManager.js
+++ src/loaders/loadingManager.js
@@ -3,6 +3,8 @@ import * as THREE from 'three'
 export const loadingManager = new THREE.LoadingManager()
 
 loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+  // console.log(itemsLoaded)
+
   const percent = Math.round((itemsLoaded / itemsTotal) * 100)
   document.querySelector('.loader-progress').textContent = percent + '%'
 }
@@ -12,4 +14,8 @@ loadingManager.onLoad = () => {
   setTimeout(() => {
     document.querySelector('.loader').style.display = 'none'
   }, 1000)
+}
+
+loadingManager.onError = (url) => {
+  console.log('There was an error loading ' + url)
 }
